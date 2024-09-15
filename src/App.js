a563e5420b60e36a873fa24ba6ff693eda9c25e6
anjan-gn <anjan.imtma@gmail.com>
2024-09-15 17:20:46 +0530

    testing removed on app.jss

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Jenkins on Udemy and S3
+          Learn Jenkins on Udemy
         </a>
       </header>
       <p>
